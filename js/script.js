f9d5420040c5a637c1bd75c9fca2a4695a4a071c
Danilo Lopes <contatolopesdan@gmail.com>
2025-02-10 14:25:40 -0300

    Add new tools

--- js/script.js
+++ js/script.js
@@ -40,3 +40,49 @@ function menuHambuger() {
 }
 
 menuHambuger();
+
+function scrollSmooth() {
+  document.querySelectorAll('.nav-links a').forEach((link) => {
+    link.addEventListemer('click', (e) => {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: 'smooth',
+        });
+      }
+    });
+  });
+}
+//caso seja criado uma nova pagina, um efeito de rolagem suave vai melhorar a experiência do usuário.
+scrollSmooth();
+
+const toggleDarkMode = document.createElement('button');
+toggleDarkMode.textContent = 'Dark Mode';
+
+// toogleDarkMode.addEventListemer('click', () => {
+//   document.body.classList.toggle('dark-mode');
+//   localStorage.setItem(
+//     'theme',
+//     document.body.classList.contains('dark-mode') ? 'dark' : 'light'
+//   );
+
+//   if (localStorage.getItem('theme') === 'dark') {
+//     document.body.classList.add('dark-mode');
+//   }
+// });
+
+function showNotification(message) {
+  const notification = document.createElement('div');
+  notification.textContent = message;
+  notification.classList.add('notification');
+  document.body.appendChild(notification);
+  setTimeout(() => {
+    notification.remove();
+  }, 3000);
+
+  document.getElementById('dowload').addEventListener('click', () => {
+    showNotification('Download started successfully');
+  });
+}
